40538a2b390afcdac6bb6daef1e4baf4a7984bf5
Lessalc <lucianoclessa@gmail.com>
2023-06-01 16:24:49 -0300

    Iniciando o projeto de Adoção de Animais
    - Projeto iniciado com o Spring Initializr
    - Adicionada depedências ao POM
    - Adicionado configuração para conexão com banco de dados
    - Adicionado primeiro comando com o Flyway

---
+++ src/main/java/com/lessalc/adopet/tutor/Tutor.java
@@ -0,0 +1,29 @@
+package com.lessalc.adopet.tutor;
+
+
+import com.lessalc.adopet.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "tutores")
+@Entity(name = "Tutores")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Tutor extends Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String sobre;
+    private String cidade;
+    private String urlFoto;
+
+
+}
---
+++ src/main/resources/db/migration/V1__creates-tables-usuarios-tutores.sql
@@ -0,0 +1,21 @@
+create table tutores(
+
+    id bigint not null auto_increment,
+    email varchar(100) not null unique,
+    senha varchar(255) not null,
+    nome varchar(100) not null,
+    cidade varchar(100),
+    sobre varchar(255),
+    telefone varchar(25),
+
+    primary key(id)
+);
+
+create table usuarios(
+
+    id bigint not null auto_increment,
+    email varchar(100) not null,
+    senha varchar(255) not null,
+    primary key(id)
+
+);
