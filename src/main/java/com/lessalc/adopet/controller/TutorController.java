430209e6416150fdca7d96c04d10eefffb100f5c
Lessalc <lucianoclessa@gmail.com>
2023-06-01 16:40:09 -0300

    Commit dado sem adicionar dados

--- src/main/java/com/lessalc/adopet/controller/TutorController.java
+++ src/main/java/com/lessalc/adopet/controller/TutorController.java
@@ -1,2 +1,18 @@
-package com.lessalc.adopet.controller;public class TutorController {
+package com.lessalc.adopet.controller;
+
+import com.lessalc.adopet.tutor.DadosCadastroTutores;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/tutores")
+public class TutorController {
+
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroTutores dados, UriComponentsBuilder uri){
+        return null;
+    }
 }
--- src/main/java/com/lessalc/adopet/tutor/DadosCadastroTutores.java
+++ src/main/java/com/lessalc/adopet/tutor/DadosCadastroTutores.java
@@ -1,2 +1,12 @@
-package com.lessalc.adopet.tutor;public record DadosCadastroTutores() {
+package com.lessalc.adopet.tutor;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record DadosCadastroTutores(
+        @NotBlank(message = "Nome é obrigatório") String nome,
+        @NotBlank @Email String email,
+        @Size(min=6) String senha
+) {
 }
